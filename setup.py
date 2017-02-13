403d97ecd7c1895d17c8316b7bfb002c3e134a11
Florian Baumann <flo@noqqe.de>
2017-02-13 15:09:02 +0100

    setup fixes

--- setup.py
+++ setup.py
@@ -17,10 +17,10 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version=read_from_file("VERSION"),
+    version=read_from_file("VERSION").strip(),
 
     description='A ssh config generator I dont recommend to use',
-    long_description=read_from_file('readme.rst'),
+    long_description=read_from_file('README.rst'),
 
     # The project's main homepage.
     url='https://github.com/noqqe/sadcat',
